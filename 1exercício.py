dd70dfd18a125b5b4718d2ab4fea4a38a90d08ee
Ismael Sousa Silva <143268454+IsmaelSousa31@users.noreply.github.com>
2024-03-09 18:37:04 -0300

    Add files via upload

---
+++ 1exercício.py
@@ -0,0 +1,15 @@
+#Colocar todas as variáveis dos prints 
+
+nome = 'João'
+sobrenome = 'Paulo'
+idade = 20
+ano_nascimento = 2024 - idade
+altura_metros = 1.76
+maior_de_idade = ano_nascimento >= 18
+
+print('Nome', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura  em metros:', altura_metros)
