42bdd57cdf31ce76e39a0e8319e0b49555576072
Ismael Sousa Silva <143268454+IsmaelSousa31@users.noreply.github.com>
2024-05-17 21:07:34 -0300

    Add files via upload

---
+++ 17_05_2024_EX_21_01_FLOAT .py
@@ -0,0 +1,8 @@
+n1 = float(input('Digite a primeira nota bimestral '))
+n2 = float(input('Digite a segunda nota bimestral '))
+n3 = float(input('Digite a terceira nota bimestral '))
+n4 = float(input('Digite a quarta nota bimestral '))
+
+media = (n1 + n2 + n3 + n4) / 4
+
+print(f'A media bimestral é: {media:.1f}')
